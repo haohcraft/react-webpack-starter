87fc87b43dc0e605cb4b24c961010098b86ea529
Hao Huang <haohcraft@gmail.com>
2014-10-30 00:30:24 -0400

    use .less instead of .less$

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css/, loader: "style-loader!css-loader" },
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+      { test: /\.less/, loader: "style-loader!css-loader!less-loader" },
       { test: /\.gif/, loader: "url-loader?limit=10000&minetype=image/gif" },
       { test: /\.jpg/, loader: "url-loader?limit=10000&minetype=image/jpg" },
       { test: /\.png/, loader: "url-loader?limit=10000&minetype=image/png" },
